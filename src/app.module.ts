dea2d20139869364715ca3f6e02d84b6444255bf
Gabriele Toselli <toselli.gabriele@gmail.com>
2024-07-10 17:11:24 +0200

    feat: add communication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { ManagementModule } from './management/management.module';
+import { CommunicationModule } from './communication/communication.module';
 
 @Module({
-  imports: [DatabaseModule, ReservationModule, ManagementModule],
+  imports: [DatabaseModule, ReservationModule, ManagementModule, CommunicationModule],
   controllers: [],
   providers: [],
 })
